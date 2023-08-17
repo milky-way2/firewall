f701f19e82fb1cda47683d39a1e73db194df040d
Soham Chakrabortty <soham61chakrabortty@gmail.com>
2023-08-18 01:31:26 +0530

    Reformatting the code style using "black"

--- sofire7/unblock.py
+++ sofire7/unblock.py
@@ -5,8 +5,10 @@ import sys
 
 def main():
     try:
-        user_input = input("Do you want to unblock an IP address? (y/n): ").strip().lower()
-        if user_input == 'y':
+        user_input = (
+            input("Do you want to unblock an IP address? (y/n): ").strip().lower()
+        )
+        if user_input == "y":
             ip_to_unblock = input("Enter the IP address to unblock: ")
             UnblockIT(ip_to_unblock)
         else:
@@ -20,5 +22,5 @@ def UnblockIT(ipaddr):
     os.popen("iptables -D INPUT -s {} -j DROP".format(ipaddr))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
